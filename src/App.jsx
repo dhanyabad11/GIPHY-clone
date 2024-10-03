c530a4b8822b1c117a080b10e5a0b285721f193e
dhanyabad11 <dhanyabadbehera@gmail.com>
2024-10-03 21:37:45 +0530

    updated the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,48 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.css';
-import AppLayout from './layouts/app-layout';
-import GifPage from './pages/single-gif';
-import Favorites from './pages/favorites';
-import Home from './pages/home';
-import Category from './pages/category';
-import SearchPage from './pages/search';
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import "./App.css";
+import AppLayout from "./layouts/app-layout";
+import Home from "./pages/home";
+import GifProvider from "./context/gif-context";
+import SearchPage from "./pages/search";
+import Category from "./pages/category";
+import GifPage from "./pages/single-gif";
+import Favorites from "./pages/favorites";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/:category',
-        element: <Category />,
-      },
-      {
-        path: '/search/:query',
-        element: <SearchPage />,
-      },
-      {
-        path: '/:type/:slug',
+        path: "/:type/:slug",
         element: <GifPage />,
       },
       {
-        path: '/favorites',
+        path: "/:category",
+        element: <Category />,
+      },
+      {
+        path: "/search/:query",
+        element: <SearchPage />,
+      },
+      {
+        path: "/favorites",
         element: <Favorites />,
       },
     ],
   },
 ]);
 
-function App() {
-  return <RouterProvider router={router} />;
-}
+const App = () => {
+  return (
+    <GifProvider>
+      <RouterProvider router={router} />
+    </GifProvider>
+  );
+};
 
 export default App;
